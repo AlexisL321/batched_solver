70dbc2aba0edc3ae7f8c377eb922cd59ba6090a9
AlexisL321 <liu2256@wisc.edu>
2024-10-23 03:10:33 -0500

    PCG

--- code/cg.py
+++ code/cg.py
@@ -43,7 +43,6 @@ def CG(x_init, A, b):
 		rk = rk1
 		pk = pk1
 		
-	
 	return xk, res_list, k
 
 def CG_smoother(x_init, A, b, smoother, num_iter):
@@ -75,8 +74,8 @@ def CG_smoother(x_init, A, b, smoother, num_iter):
 			break
 
 		# preconditioning using pointwise Jacobi
-		b_p = np.zeros(b.shape)
-		rk1_p,_,_ = pointwise.pointwise_solver(rk1, A, b_p, smoother,
+		#b_p = np.zeros(b.shape)
+		rk1_p,_,_ = pointwise.pointwise_solver(rk1, A, rk1, smoother,
 			max_iter=num_iter)
 
 		beta_k = rk1_p.T.dot(rk1)/rk1_p.T.dot(rk)
@@ -85,9 +84,53 @@ def CG_smoother(x_init, A, b, smoother, num_iter):
 		print(k, ": ",res)
 		k = k+1
 		xk = xk1
-		rk = rk1
+		rk = rk1_p
+		pk = pk1
+	
+	return xk, res_list, k
+
+def CG_smoother_blc(x_init, A, b, bsize, num_blc, solver, num_iter):
+	x0 = x_init.copy()
+	r0 = sps.csc_matrix(b - A.dot(x0))
+	p0 = r0
+	k = 0
+	rk = r0
+	rk1_p = rk
+	pk = p0
+	xk = x0
+	#print(sp.linalg.norm(b).shape)
+	tol = 0
+	if sps.issparse(b):
+		tol = 1e-6*sps.linalg.norm(b)
+	else:
+		tol = 1e-6*np.linalg.norm(b)
+	print(tol)
+	res_list = []
+	res = 0
+
+	while True:
+		alpha_k = rk1_p.T.dot(rk)/pk.T.dot(A.dot(pk))
+		xk1 = xk + pk.multiply(alpha_k)
+		rk1 = rk - A.dot(pk).multiply(alpha_k)
+		res = sps.linalg.norm(rk1)
+		res_list.append(res)
+		if res < tol:
+			break
+
+		# preconditioning using pointwise Jacobi
+		#b_p = np.zeros(b.shape)
+		rk1_p,_,_ = block.blc_jacobi(rk1, A, rk1, bsize, num_blc, solver,
+			max_iter=num_iter)
+
+		beta_k = rk1_p.T.dot(rk1)/rk1_p.T.dot(rk)
+		pk1 = rk1_p + pk.multiply(beta_k)
+
+		print(k, ": ",res)
+		k = k+1
+		xk = xk1
+		# why rk =rk1 doesn't converge?
+		rk = rk1_p
 		pk = pk1
 		
-	
 	return xk, res_list, k
 
--- code/driver.py
+++ code/driver.py
@@ -29,7 +29,7 @@ def main():
 	'''
 
 	# test with a smaller matrix
-	n_test = 100
+	n_test = 3000
 	mat1 = sps.random(n_test, n_test, density=0.3, format='csc')
 	for i in range(n_test):
 		mat1[i,i] += n_test*0.25
@@ -48,13 +48,15 @@ def main():
 	#list_res_g, num_iter_g = pointwise.GaussSeidel(x_init, mat1, b)
 
 	# try block algorithms
-	bsize = 10
-	num_blc = 10
+	bsize = 100
+	num_blc = 30
 	solver = 'sparse_chol'
 	#list_res_j, num_iter_j = block.blc_jacobi(x_init, mat1, b, bsize,
 		#num_blc, solver)
 	#result, list_res_cg, num_iter_g = cg.CG_smoother(x_init, mat1, b, 'j', 3)
 	_,list_res_cg, num_iter_g = cg.CG(x_init, mat1, b)
+	_,list_res_pcg, num_iter_pg = cg.CG_smoother_blc(x_init, mat1, b, bsize,
+	num_blc, solver, 10)
 	#list_res_g, num_iter_g = block.blc_GaussSeidel(x_init, mat1, b, bsize,
 	#num_blc, solver)
 
@@ -62,6 +64,7 @@ def main():
 	#plt.plot(list_res_j, label='jacobi', color='red')
 	#plt.plot(list_res_g, label='GS', color='blue')
 	plt.plot(list_res_cg, label='CG', color='green')
+	plt.plot(list_res_pcg, label='PCG', color='red')
 
 	plt.ylabel('residual')
 	plt.xlabel('iteration')
